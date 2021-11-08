b37d202b17f68624d9a4a9cef87e463ea3afc1fd
Lal0gg <2993815280101@ingenieria.usac.edu.gt>
2021-11-07 21:51:48 -0600

    se actualizo el backnd

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def mostrarUsers():
     return(jsonify(Datos)) 
 
 #Login usuario
-
+'''
 @app.route('/Logiin',methods =['POST'])
 def obtenerUnUsuario():
     global Usuarios
@@ -44,7 +44,7 @@ def obtenerUnUsuario():
                 envios.append(unEnvio)
     respuesta = jsonify(envios)   
     return respuesta
-
+'''
 
 #Login Admin
 @app.route('/Login',methods =['POST'])
